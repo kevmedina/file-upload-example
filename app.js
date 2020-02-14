120990bfba41cb15ddd778207f438e9a21568c1b
Kevin Medina <kmedinakm@hotmail.com>
2020-02-13 23:33:47 -0500

    update css and add config file for mongoose database

--- app.js
+++ app.js
@@ -9,15 +9,7 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
-
-mongoose
-  .connect('mongodb://localhost/file-upload-example', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+require('./config/db.config');
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -50,7 +42,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.locals.title = 'Express - Generated with IronGenerator';
 
 
-
+// Routes
 const index = require('./routes/index');
 app.use('/', index);
 app.use('/dragons', require('./routes/dragon-routes/dragon'));
